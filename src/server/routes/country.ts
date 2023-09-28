f131d364ff6cd41de00f8153e76190dddf4adb5b
erikao1998 <ersku1998@gmail.com>
2023-09-28 13:01:17 +0300

    Fetch HCI data

--- src/server/routes/country.ts
+++ src/server/routes/country.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import getCountries from '../data/worldbank/countries'
 import getCountryCorruption from '../data/worldbank/corruption'
 import getCountryPoliticalStability from '../data/worldbank/politicalStability'
+import getCountryHCI from '../data/worldbank/humanCapitalIndex'
 
 const countryRouter = express.Router()
 
@@ -43,4 +44,16 @@ countryRouter.get('/:code/stability', async (req, res: any) => {
   return res.status(200).send(country)
 })
 
+countryRouter.get('/:code/hci', async (req, res: any) => {
+  const { code } = req.params
+
+  const hci = await getCountryHCI(code)
+
+  const country = {
+    code,
+    hci,
+  }
+
+  return res.status(200).send(country)
+})
 export default countryRouter
