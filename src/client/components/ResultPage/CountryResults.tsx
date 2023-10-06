56bfb82c26af8fd76ad919d50d5264f4b81f42f7
erikao1998 <ersku1998@gmail.com>
2023-10-06 14:57:51 +0300

    Add risk calculation algorithm for countries

--- src/client/components/ResultPage/CountryResults.tsx
+++ src/client/components/ResultPage/CountryResults.tsx
@@ -8,20 +8,46 @@ import styles from '../../styles'
 
 const { resultStyles } = styles
 
+const riskLevelCheck = (
+  start: number,
+  end: number,
+  intervals: number,
+  res: number
+) => {
+  const riskLevel = ['Erittäin huono', 'Huono', 'Hyvä', 'Erittäin hyvä']
+  const distance = Math.abs(start - end)
+  const intervalCounter = distance / intervals
+
+  let startOfInterval = start
+  let endOfInterval = start + intervalCounter
+  for (let step = 0; step < intervals; step += 1) {
+    if (res > startOfInterval && res < endOfInterval) {
+      return riskLevel[step]
+    }
+    startOfInterval = start + intervalCounter
+    endOfInterval += intervalCounter
+  }
+  return ''
+}
+
 const CountryResults = ({ selectedCountryCode }: any) => {
   const { country } = useCountry(selectedCountryCode)
   const { t } = useTranslation()
 
+  if (!country) return null
+
   return (
     <>
       <Box sx={resultStyles.card}>
-        {t('results:corruptionRank')}: {country?.corruption}
+        {t('results:corruptionRank')}:{' '}
+        {riskLevelCheck(-2.5, 2.5, 4, country.corruption)}
       </Box>
       <Box sx={resultStyles.card}>
-        {t('results:stabilityRank')}: {country?.stability}
+        {t('results:stabilityRank')}:{' '}
+        {riskLevelCheck(0, 100, 4, country.stability)}
       </Box>
       <Box sx={resultStyles.card}>
-        {t('results:HCIrank')}: {country?.hci}
+        {t('results:HCIrank')}: {riskLevelCheck(0, 1, 4, country.hci)}
       </Box>
       <Box sx={resultStyles.card}>
         {t('results:safetyLevel')}: {country?.safetyLevel}
