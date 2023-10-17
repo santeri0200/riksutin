1662d8674ff2ba392b4042c8bd4915c60e6674eb
erikao1998 <ersku1998@gmail.com>
2023-10-17 19:23:36 +0300

    Update questions

--- src/server/data/questions.ts
+++ src/server/data/questions.ts
@@ -15,9 +15,9 @@ const getQuestionData = async (): Promise<Question[]> => [
     parentId: null,
     priority: 0,
     title: {
-      fi: 'Nimi',
-      sv: 'Namn',
-      en: 'Name',
+      fi: 'Ilmoittajan nimi',
+      sv: 'Name of the person providing information',
+      en: 'Name of the person providing information',
     },
     text: {
       fi: '',
@@ -33,9 +33,9 @@ const getQuestionData = async (): Promise<Question[]> => [
     parentId: null,
     priority: 1,
     title: {
-      fi: 'Yksikkö',
-      sv: 'Yksikkö',
-      en: 'Yksikkö',
+      fi: 'Yhteistyön vastuuyksikkö HY:ssa',
+      sv: 'UH unit responsible for collaboration',
+      en: 'UH unit responsible for collaboration',
     },
     text: {
       fi: '',
@@ -51,9 +51,9 @@ const getQuestionData = async (): Promise<Question[]> => [
     parentId: null,
     priority: 2,
     title: {
-      fi: 'Yhteistyöprojektin nimi',
-      sv: 'Yhteistyöprojektin nimi',
-      en: 'Yhteistyöprojektin nimi',
+      fi: 'Yhteistyön/yhteistyöprojektin nimi',
+      sv: 'Name of the collaboration/project',
+      en: 'Name of the collaboration/project',
     },
     text: {
       fi: '',
@@ -67,29 +67,51 @@ const getQuestionData = async (): Promise<Question[]> => [
     id: 4,
     surveyId: 1,
     parentId: null,
-    priority: 4,
+    priority: 3,
     title: {
-      fi: 'Yhteistyöprojektin kumppanin nimi',
-      sv: 'Yhteistyöprojektin kumppanin nimi',
-      en: 'Yhteistyöprojektin kumppanin nimi',
+      fi: 'Yhteistyökonsortion koostumus',
+      sv: 'Composition of consortium',
+      en: 'Composition of consortium',
     },
     text: {
       fi: '',
       sv: '',
       en: '',
     },
-    optionData: { type: 'text', options: [] },
+    optionData: {
+      type: 'singleChoice',
+      options: [
+        {
+          id: 'bilateral',
+          label: 'bilateral',
+          title: {
+            fi: 'Kahdenvälinen',
+            sv: 'Bilateral',
+            en: 'Bilateral',
+          },
+        },
+        {
+          id: 'multilateral',
+          label: 'multilateral',
+          title: {
+            fi: 'Monenkeskeinen',
+            sv: 'Multilateral',
+            en: 'Multilateral',
+          },
+        },
+      ],
+    },
     visibility: {},
   },
   {
     id: 5,
     surveyId: 1,
     parentId: null,
-    priority: 3,
+    priority: 4,
     title: {
-      fi: 'Projektin lyhyt kuvaus',
-      sv: 'Projektin lyhyt kuvaus',
-      en: 'Projektin lyhyt kuvaus',
+      fi: 'Yhteistyöorganisaation nimi',
+      sv: 'Name of the partner organisation',
+      en: 'Name of the partner organisation',
     },
     text: {
       fi: '',
@@ -103,176 +125,11 @@ const getQuestionData = async (): Promise<Question[]> => [
     id: 6,
     surveyId: 1,
     parentId: null,
-    priority: 9,
-    title: {
-      fi: 'Kirjallinen sopimus',
-      sv: 'Kirjallinen sopimus',
-      en: 'Kirjallinen sopimus',
-    },
-    text: {
-      fi: '',
-      sv: '',
-      en: '',
-    },
-    optionData: {
-      type: 'singleChoice',
-      options: [
-        {
-          id: 'yes',
-          label: 'yes',
-          title: {
-            fi: 'Kyllä',
-            sv: 'Kyllä',
-            en: 'Kyllä',
-          },
-        },
-        {
-          id: 'no',
-          label: 'no',
-          title: {
-            fi: 'Ei',
-            sv: 'Ei',
-            en: 'Ei',
-          },
-        },
-      ],
-    },
-    visibility: {},
-  },
-  {
-    id: 7,
-    surveyId: 1,
-    parentId: null,
-    priority: 7,
-    title: {
-      fi: 'Yhteistyön muodot',
-      sv: 'Yhteistyön muodot',
-      en: 'Yhteistyön muodot',
-    },
-    text: {
-      fi: '',
-      sv: '',
-      en: '',
-    },
-    optionData: {
-      type: 'multipleChoice',
-      options: [
-        {
-          id: 'option1',
-          label: 'option1',
-          title: {
-            fi: 'Tutkimus',
-            sv: 'Tutkimus',
-            en: 'Tutkimus',
-          },
-        },
-        {
-          id: 'option2',
-          label: 'option2',
-          title: {
-            fi: 'Koulutus',
-            sv: 'Koulutus',
-            en: 'Koulutus',
-          },
-        },
-        {
-          id: 'option3',
-          label: 'option3',
-          title: {
-            fi: 'Koulutusvienti',
-            sv: 'Koulutusvienti',
-            en: 'Koulutusvienti',
-          },
-        },
-        {
-          id: 'option4',
-          label: 'option4',
-          title: {
-            fi: 'Kansainvälinen opiskelijaliikkuvuus',
-            sv: 'Kansainvälinen opiskelijaliikkuvuus',
-            en: 'Kansainvälinen opiskelijaliikkuvuus',
-          },
-        },
-        {
-          id: 'option5',
-          label: 'option5',
-          title: {
-            fi: 'Kansainvälinen henkilöstöliikkuvuus',
-            sv: 'Kansainvälinen henkilöstöliikkuvuus',
-            en: 'Kansainvälinen henkilöstöliikkuvuus',
-          },
-        },
-        {
-          id: 'option6',
-          label: 'option6',
-          title: {
-            fi: 'Yhteistutkinto',
-            sv: 'Yhteistutkinto',
-            en: 'Yhteistutkinto',
-          },
-        },
-        {
-          id: 'option7',
-          label: 'option7',
-          title: {
-            fi: 'Etäkampus',
-            sv: 'Etäkampus',
-            en: 'Etäkampus',
-          },
-        },
-      ],
-    },
-    visibility: {},
-  },
-  {
-    id: 8,
-    surveyId: 1,
-    parentId: 7,
-    priority: 0,
-    title: {
-      fi: 'Liikkuvuuden täsmennys',
-      sv: 'Liikkuvuuden täsmennys',
-      en: 'Liikkuvuuden täsmennys',
-    },
-    text: {
-      fi: '',
-      sv: '',
-      en: '',
-    },
-    optionData: {
-      type: 'singleChoice',
-      options: [
-        {
-          id: 'childOption1',
-          label: 'childOption1',
-          title: {
-            fi: 'Vaihtoehto A',
-            sv: 'Alternativ A',
-            en: 'Option A',
-          },
-        },
-        {
-          id: 'childOption2',
-          label: 'childOption2',
-          title: {
-            fi: 'Vaihtoehto B',
-            sv: 'Alternativ B',
-            en: 'Option B',
-          },
-        },
-      ],
-    },
-    visibility: { options: ['option4', 'option5'] },
-  },
-  {
-    id: 9,
-    surveyId: 1,
-    parentId: null,
     priority: 5,
     title: {
-      fi: 'Yhteistyökumppanin tyyppi',
-      sv: 'Yhteistyökumppanin tyyppi',
-      en: 'Yhteistyökumppanin tyyppi',
+      fi: 'Yhteistyöorganisaation tyyppi',
+      sv: 'Name of the partner organisation',
+      en: 'Name of the partner organisation',
     },
     text: {
       fi: '',
@@ -286,14 +143,50 @@ const getQuestionData = async (): Promise<Question[]> => [
     visibility: {},
   },
   {
-    id: 10,
+    id: 7,
+    surveyId: 1,
+    parentId: null,
+    priority: 6,
+    title: {
+      fi: 'Vapaa sana',
+      sv: 'Free word',
+      en: 'Free word',
+    },
+    text: {
+      fi: '',
+      sv: '',
+      en: '',
+    },
+    optionData: { type: 'text', options: [] },
+    visibility: {},
+  },
+  {
+    id: 8,
+    surveyId: 1,
+    parentId: null,
+    priority: 7,
+    title: {
+      fi: 'Yhteistyökumppanin sijaintimaa',
+      sv: 'Yhteistyöprojektin sijaintimaa',
+      en: 'Yhteistyöprojektin sijaintimaa',
+    },
+    text: {
+      fi: '',
+      sv: '',
+      en: '',
+    },
+    optionData: { type: 'select', options: await getCountryNames() },
+    visibility: {},
+  },
+  {
+    id: 9,
     surveyId: 1,
     parentId: null,
     priority: 8,
     title: {
-      fi: 'Helsingin yliopiston asema projektissa',
-      sv: 'Helsingin yliopiston asema projektissa',
-      en: 'Helsingin yliopiston asema projektissa',
+      fi: 'Helsingin yliopiston asema yhteistyössä',
+      sv: 'Role of UH in collaboration',
+      en: 'Role of UH in collaboration',
     },
     text: {
       fi: '',
@@ -312,22 +205,224 @@ const getQuestionData = async (): Promise<Question[]> => [
     visibility: {},
   },
   {
-    id: 11,
+    id: 10,
     surveyId: 1,
     parentId: null,
-    priority: 6,
+    priority: 9,
     title: {
-      fi: 'Yhteistyökumppanin sijaintimaa',
-      sv: 'Yhteistyöprojektin sijaintimaa',
-      en: 'Yhteistyöprojektin sijaintimaa',
+      fi: 'Yhteistyösopimus tekeillä tai tehty',
+      sv: 'Written agreement is or will be made',
+      en: 'Written agreement is or will be made',
     },
     text: {
       fi: '',
       sv: '',
       en: '',
     },
-    optionData: { type: 'select', options: await getCountryNames() },
+    optionData: {
+      type: 'singleChoice',
+      options: [
+        {
+          id: 'agreementDone',
+          label: 'agreementDone',
+          title: {
+            fi: 'Kyllä',
+            sv: 'Yes',
+            en: 'Yes',
+          },
+        },
+        {
+          id: 'agreementNotDone',
+          label: 'agreementNotDone',
+          title: {
+            fi: 'Ei',
+            sv: 'No',
+            en: 'No',
+          },
+        },
+      ],
+    },
     visibility: {},
+  },
+
+  {
+    id: 11,
+    surveyId: 1,
+    parentId: null,
+    priority: 10,
+    title: {
+      fi: 'Yhteistyön muodot',
+      sv: 'Yhteistyön muodot',
+      en: 'Yhteistyön muodot',
+    },
+    text: {
+      fi: '',
+      sv: '',
+      en: '',
+    },
+    optionData: {
+      type: 'multipleChoice',
+      options: [
+        {
+          id: 'research',
+          label: 'research',
+          title: {
+            fi: 'Tutkimusyhteistyö',
+            sv: 'Research collaboration',
+            en: 'Research collaboration',
+          },
+        },
+        {
+          id: 'education',
+          label: 'education',
+          title: {
+            fi: 'Koulutus/opetusyhteistyö',
+            sv: 'Educational collaboration',
+            en: 'Educational collaboration',
+          },
+        },
+        {
+          id: 'educationExport',
+          label: 'educationExport',
+          title: {
+            fi: 'Koulutusvienti',
+            sv: 'Export of Education',
+            en: 'Export of Education',
+          },
+        },
+        {
+          id: 'studentMobility',
+          label: 'studentMobility',
+          title: {
+            fi: 'Kansainvälinen opiskelijaliikkuvuus',
+            sv: 'International student mobility',
+            en: 'International student mobility',
+          },
+        },
+        {
+          id: 'staffMobility',
+          label: 'staffMobility',
+          title: {
+            fi: 'Kansainvälinen henkilöstöliikkuvuus',
+            sv: 'International faculty or staff mobility',
+            en: 'International faculty or staff mobility',
+          },
+        },
+        {
+          id: 'jointDegree',
+          label: 'jointDegree',
+          title: {
+            fi: 'Yhteistutkintoyhteistyö',
+            sv: 'Joint or Double Degree',
+            en: 'Joint or Double Degree',
+          },
+        },
+        {
+          id: 'branchCampus',
+          label: 'branchCampus',
+          title: {
+            fi: 'Etäkampus',
+            sv: 'Branch Campus',
+            en: 'Branch Campus',
+          },
+        },
+      ],
+    },
+    visibility: {},
+  },
+  {
+    id: 12,
+    surveyId: 1,
+    parentId: null,
+    priority: 11,
+    title: {
+      fi: 'Yhteistyön kesto vuosina',
+      sv: 'Duration of collaboration in years',
+      en: 'Duration of collaboration in years',
+    },
+    text: {
+      fi: '',
+      sv: '',
+      en: '',
+    },
+    optionData: { type: 'text', options: [] },
+    visibility: {},
+  },
+  {
+    id: 13,
+    surveyId: 1,
+    parentId: null,
+    priority: 12,
+    title: {
+      fi: 'Sisältyykö yhteistyöhön ulkopuolista rahoitusta?',
+      sv: 'Is external funding included in this collaboration?',
+      en: 'Is external funding included in this collaboration?',
+    },
+    text: {
+      fi: '',
+      sv: '',
+      en: '',
+    },
+    optionData: {
+      type: 'singleChoice',
+      options: [
+        {
+          id: 'externalFunding',
+          label: 'externalFunding',
+          title: {
+            fi: 'Kyllä',
+            sv: 'Yes',
+            en: 'Yes',
+          },
+        },
+        {
+          id: 'noExternalFunding',
+          label: 'noExternalFunding',
+          title: {
+            fi: 'Ei',
+            sv: 'No',
+            en: 'No',
+          },
+        },
+      ],
+    },
+    visibility: {},
+  },
+  {
+    id: 14,
+    surveyId: 1,
+    parentId: 13,
+    priority: 0,
+    title: {
+      fi: 'Rahoittajan nimi',
+      sv: 'Name of funding agency',
+      en: 'Name of funding agency',
+    },
+    text: {
+      fi: '',
+      sv: '',
+      en: '',
+    },
+    optionData: { type: 'text', options: [] },
+    visibility: { options: ['externalFunding'] },
+  },
+  {
+    id: 15,
+    surveyId: 1,
+    parentId: 13,
+    priority: 1,
+    title: {
+      fi: 'Ulkopuolisen rahoituksen määrä',
+      sv: 'Amount of external funding',
+      en: 'Amount of external funding',
+    },
+    text: {
+      fi: '',
+      sv: '',
+      en: '',
+    },
+    optionData: { type: 'text', options: [] },
+    visibility: { options: ['externalFunding'] },
   },
 ]
 
