7550ce8936409adc040e01aedfed38ef6bf273b8
erikao1998 <ersku1998@gmail.com>
2023-10-20 11:59:07 +0300

    Set default value for name field

--- src/client/components/Choices/TextField.tsx
+++ src/client/components/Choices/TextField.tsx
@@ -3,31 +3,33 @@ import { Controller } from 'react-hook-form'
 import { Box, TextField } from '@mui/material'
 
 import { InputProps } from '../../types'
+import useLoggedInUser from '../../hooks/useLoggedInUser'
 
-const Text = ({ control, question, children }: InputProps) => {
+const Text = ({ control, question }: InputProps) => {
+  const { user } = useLoggedInUser()
+
   if (!question) return null
 
   const props = question.optionData.options.find(({ attributes }) => attributes)
 
   return (
-    <>
-      <Controller
-        control={control}
-        name={question.id.toString()}
-        defaultValue=""
-        render={({ field: { onChange } }) => (
-          <Box justifyContent="center">
-            <TextField
-              onChange={onChange}
-              fullWidth
-              InputProps={props ? props.attributes : {}}
-            />
-          </Box>
-        )}
-      />
-
-      {children}
-    </>
+    <Controller
+      control={control}
+      name={question.id.toString()}
+      defaultValue=""
+      render={({ field: { onChange } }) => (
+        <Box justifyContent="center">
+          <TextField
+            onChange={onChange}
+            fullWidth
+            InputProps={props ? props.attributes : {}}
+            defaultValue={
+              question.id === 1 ? `${user?.firstName} ${user?.lastName}` : ''
+            }
+          />
+        </Box>
+      )}
+    />
   )
 }
 
