ad3772b5e5393838468753b9a551223ff07064a8
erikao1998 <ersku1998@gmail.com>
2023-11-14 15:49:52 +0200

    Show all answers in the results page

--- src/client/components/ResultPage/RenderResults.tsx
+++ src/client/components/ResultPage/RenderResults.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { Locales } from '@backend/types'
 import { Box } from '@mui/material'
 
 import useSurvey from '../../hooks/useSurvey'
@@ -13,6 +15,7 @@ import useCountries from '../../hooks/useCountries'
 const { resultStyles } = styles
 
 const RenderResults = () => {
+  const { i18n } = useTranslation()
   const { survey } = useSurvey()
   const { countries, isLoading } = useCountries()
   const refCallback = useResultRefCallback()
@@ -21,13 +24,86 @@ const RenderResults = () => {
 
   if (isLoading || !countries || !survey || !resultData) return null
 
+  const { language } = i18n
   const selectedCountry: any = resultData['8']
   const selectedCountryCode = countries.find(
     (country) => country.name === selectedCountry
   )?.code
 
+  const multiChoiceQuestions = survey.Questions.filter(
+    (question) => question.optionData.type === 'multipleChoice'
+  )
+
+  const singleChoiceQuestions = survey.Questions.filter(
+    (question) => question.optionData.type === 'singleChoice'
+  )
+
+  const singleChoiceAnswers = singleChoiceQuestions.map((question) => {
+    const questionId = question.id
+    const answer = resultData[questionId]
+    const text = question.optionData.options.find((o) => o.id === answer)
+      ?.title[language as keyof Locales]
+
+    return { [questionId]: text || '' }
+  })
+
+  const multiChoiceAnswers = multiChoiceQuestions.map((question) => {
+    const questionId = question.id
+    const answer = resultData[questionId]
+    const trues = Object.entries(answer)
+      .filter((pair) => pair[1] === true)
+      .map((pair) => pair[0])
+      .flat(1)
+    const texts = trues.map(
+      (value) =>
+        question.optionData.options.find((o) => o.id === value)?.title[
+          language as keyof Locales
+        ]
+    )
+
+    return { [questionId]: texts.join(', ') || '' }
+  })
+
+  const answers = {
+    ...resultData,
+    ...Object.assign({}, ...singleChoiceAnswers),
+    ...Object.assign({}, ...multiChoiceAnswers),
+  }
+
   return (
     <Box ref={refCallback}>
+      <Box sx={resultStyles.resultElementWrapper}>
+        {survey?.Questions.map((question) => (
+          <Box key={question.id}>
+            {question.parentId === null ? (
+              <>
+                <Box sx={resultStyles.card}>
+                  {question.title[language as keyof Locales]}:{' '}
+                  {answers[question.id] as string}
+                </Box>
+                {survey?.Questions.filter(
+                  (q) => q.parentId === question.id
+                )?.map((q) => (
+                  <Box key={q.id}>
+                    {answers[q.id] ? (
+                      <Box
+                        sx={{
+                          m: 2,
+                          borderLeft: 1,
+                          margin: '15px',
+                          paddingLeft: '10px',
+                        }}
+                      >
+                        {q.title[language as keyof Locales]}: {answers[q.id]}
+                      </Box>
+                    ) : null}
+                  </Box>
+                ))}
+              </>
+            ) : null}
+          </Box>
+        ))}
+      </Box>
       <Box sx={resultStyles.resultElementWrapper}>
         <TotalRisk
           selectedCountryCode={selectedCountryCode}
