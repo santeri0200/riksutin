2bbfe53a545501125f3facf3588a0247f3b92e6e
erikao1998 <ersku1998@gmail.com>
2024-04-19 13:32:08 +0300

    Create function that takes care of the algorithm stuff

--- src/client/util/algorithm/getRiskValues.ts
+++ src/client/util/algorithm/getRiskValues.ts
@@ -1,13 +1,35 @@
-import { FormValues, Question, Result } from '@backend/types'
+import { Question, Result } from '@backend/types'
+import { Country, CountryData, FormValues } from '../../types'
+import apiClient from '../apiClient'
 
-const getRiskValues = (
-  data: FormValues,
+import totalRisk from './totalRisk'
+
+const getRiskValues = async (
+  formdata: FormValues,
   questions: Question[],
   results: Result[],
   language: string
 ) => {
-  console.log(data)
-  return data
+  const countries: Country[] = (await apiClient.get('/countries')).data
+
+  const selectedCountry: any = formdata['8']
+  const selectedCountryCode = countries?.find(
+    (country) => country.name === selectedCountry
+  )?.code
+
+  const country: CountryData = (
+    await apiClient.get(`/countries/${selectedCountryCode}`)
+  ).data
+
+  const { totalRiskLevel, filteredArray } = totalRisk(
+    country,
+    questions,
+    results,
+    formdata,
+    language
+  )
+
+  return formdata
 }
 
 export default getRiskValues
