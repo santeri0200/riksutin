8e4c419842d002ea62085dd2fa072a467c7a652b
erikao1998 <ersku1998@gmail.com>
2024-05-03 13:20:25 +0300

    Multichoice component can now also be made mandatory

--- src/client/components/QuestionTypes/MultiChoice.tsx
+++ src/client/components/QuestionTypes/MultiChoice.tsx
@@ -1,53 +1,60 @@
 import React from 'react'
 import { Controller } from 'react-hook-form'
-import { Box, FormControl, Checkbox, FormControlLabel } from '@mui/material'
+import {
+  Box,
+  FormControl,
+  Checkbox,
+  FormControlLabel,
+  FormHelperText,
+} from '@mui/material'
 
 import { MultipleChoiceType, Locales } from '@backend/types'
 
-import ShowMore from '../Common/ShowMore'
-
 import { InputProps } from '../../types'
 
-import styles from '../../styles'
-
 const MultiChoice = ({ control, question, children, language }: InputProps) => {
-  const { formStyles } = styles
-
   if (!question) return null
 
   return (
     <>
-      {(question.optionData.options as MultipleChoiceType[]).map(
-        (choice: MultipleChoiceType) => (
-          <Controller
-            key={choice.id}
-            name={`${question.id}.${choice.id}`}
-            control={control}
-            defaultValue={false}
-            render={({ field }) => (
-              <FormControl sx={formStyles.formControl}>
-                <Box sx={formStyles.choiceBox}>
+      <Controller
+        name={question.id.toString()}
+        control={control}
+        defaultValue={[]}
+        rules={{ required: { value: true, message: 'required' } }}
+        render={({ field, fieldState: { error } }) => (
+          <Box>
+            <FormControl>
+              {(question.optionData.options as MultipleChoiceType[]).map(
+                (choice: MultipleChoiceType) => (
                   <FormControlLabel
+                    key={choice.id}
+                    value={choice.id}
                     control={
                       <Checkbox
-                        {...field}
-                        value={choice.id}
-                        checked={field.value}
+                        onChange={() => {
+                          if (!field.value.includes(choice.id)) {
+                            field.onChange([...field.value, choice.id])
+                            return
+                          }
+                          const newValues = field.value.filter(
+                            (c: string) => c !== choice.id
+                          )
+                          field.onChange(newValues)
+                        }}
                       />
                     }
-                    data-cy={`choice-select-${choice.id}`}
                     label={choice.title[language as keyof Locales]}
                   />
-                  {choice.data ? (
-                    <ShowMore text={choice.data[language as keyof Locales]} />
-                  ) : null}
-                </Box>
-              </FormControl>
-            )}
-          />
-        )
-      )}
-
+                )
+              )}
+              <FormHelperText error>
+                {error ? error.message : null}
+              </FormHelperText>
+            </FormControl>
+          </Box>
+        )}
+      />
       {children}
     </>
   )
