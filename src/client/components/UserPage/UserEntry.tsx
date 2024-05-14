8f599c4fd3be8815f79e76aa3a0fdea5989874fe
erikao1998 <ersku1998@gmail.com>
2024-05-14 09:39:52 +0300

    Move risk table into a tabs component in the user page

--- src/client/components/UserPage/UserEntry.tsx
+++ src/client/components/UserPage/UserEntry.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Box } from '@mui/material'
+/* eslint-disable react/no-array-index-key */
+import React, { useState } from 'react'
+import { Box, Tab, Tabs } from '@mui/material'
 
 import { useParams } from 'react-router-dom'
 import RenderAnswers from '../ResultPage/RenderAnswers'
@@ -10,10 +11,41 @@ import styles from '../../styles'
 
 const { formStyles } = styles
 
+interface TabPanelProps {
+  children: React.ReactNode
+  index: number
+  value: number
+}
+
+const formatDate = (dateString: string) =>
+  `${new Date(dateString).toLocaleDateString()} ${new Date(
+    dateString
+  ).toLocaleTimeString()}`
+
+const TabPanel = (props: TabPanelProps) => {
+  const { children, value, index } = props
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  )
+}
+
 const UserEntry = () => {
   const { entryId } = useParams()
   const { survey } = useSurvey()
   const { entry } = useEntry(entryId)
+  const [tabValue, setTabValue] = useState(0)
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue)
+  }
 
   if (!entry || !survey) return null
 
@@ -21,7 +53,28 @@ const UserEntry = () => {
 
   return (
     <Box sx={formStyles.formWrapper}>
-      {risks && country && <RiskTable riskData={entry.data} />}
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={tabValue} onChange={handleChange}>
+          {country.map((countryData, index) => (
+            <Tab
+              key={index}
+              sx={{ color: 'black' }}
+              label={formatDate(countryData.createdAt || entry.createdAt)}
+            />
+          ))}
+        </Tabs>
+      </Box>
+      {risks &&
+        country &&
+        country.map((countryData, index) => (
+          <TabPanel key={index} value={tabValue} index={index}>
+            <RiskTable
+              key={index}
+              riskData={entry.data}
+              countryData={countryData}
+            />
+          </TabPanel>
+        ))}
       <RenderAnswers survey={survey} resultData={answers} />
     </Box>
   )
