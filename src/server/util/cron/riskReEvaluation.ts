53da291a6c3d8f76e6c54c342a48184fbf094db0
erikao1998 <ersku1998@gmail.com>
2024-05-28 13:18:59 +0300

    Small text change in the cron job

--- src/server/util/cron/riskReEvaluation.ts
+++ src/server/util/cron/riskReEvaluation.ts
@@ -68,7 +68,7 @@ const run = async () => {
 
 const startRiskCron = async () => {
   const cronTime = '0 12 * * 1'
-  logger.info('Cron job activated')
+  logger.info('Cron job scheduled')
   return scheduleCronJob(cronTime, run)
 }
 
