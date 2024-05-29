9f67044c1e51ea3baca1186a266849609efc028a
erikao1998 <ersku1998@gmail.com>
2024-05-29 11:54:50 +0300

    Try material-react-table in the admin summary page

--- src/client/components/Admin/Summary/Summary.tsx
+++ src/client/components/Admin/Summary/Summary.tsx
@@ -1,24 +1,56 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import {
-  Box,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Typography,
-} from '@mui/material'
-import { Link } from 'react-router-dom'
+  MaterialReactTable,
+  useMaterialReactTable,
+  type MRT_ColumnDef,
+} from 'material-react-table'
 import { useTranslation } from 'react-i18next'
-import styles from '../../../styles'
+import { Box, Typography } from '@mui/material'
 import { useEntries } from '../../../hooks/useEntry'
+import useQuestions from '../../../hooks/useQuestions'
+// example data type
 
-const { riskColors, resultStyles } = styles
+type TableValues = {
+  projectName: string
+  date: string
+  user: string
+  total: number
+}
+const columnNames = {
+  projectName: 'Yhteistyöprojektin nimi',
+  date: 'Päivämäärä',
+  user: 'Käyttäjä',
+  total: 'Kokonaisriskitaso',
+} as const
+
+const Table = ({ tableValues }: { tableValues: TableValues[] }) => {
+  const columns = useMemo<MRT_ColumnDef<TableValues>[]>(
+    () =>
+      tableValues.length
+        ? Object.keys(tableValues[0]).map((columnId) => ({
+            header: columnNames[columnId as keyof TableValues] ?? columnId,
+            accessorKey: columnId,
+            id: columnId,
+          }))
+        : [],
+    [tableValues]
+  )
+
+  const table = useMaterialReactTable({
+    columns,
+    data: tableValues,
+  })
+
+  return <MaterialReactTable table={table} />
+}
 
 const Summary = () => {
   const { t } = useTranslation()
   const { entries } = useEntries()
+  const { questions } = useQuestions(1)
+
+  if (!questions) return null
 
   const entriesWithData = entries?.filter(
     (entry) => entry.data.answers && entry.data.country && entry.data.risks
@@ -33,72 +65,16 @@ const Summary = () => {
       </Box>
     )
 
-  return (
-    <Box sx={{ mx: 2, mt: 4 }}>
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h5" sx={{ my: 4, pl: 1 }}>
-          Kaikki täytetyt kyselyt
-        </Typography>
-      </Box>
-      <TableContainer sx={{ m: 2 }}>
-        <Table sx={{ maxWidth: '40rem' }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Yhteistyöprojektin nimi</TableCell>
-              <TableCell align="center">Päivämäärä</TableCell>
-              <TableCell align="center">Käyttäjä</TableCell>
-              <TableCell align="center">Kokonaisriskitaso</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {entriesWithData.map((entry) => (
-              <TableRow key={entry.id}>
-                <TableCell component="th" scope="row">
-                  <Link to={`/user/${entry.id.toString()}`}>
-                    {entry.data.answers['3']}
-                  </Link>
-                </TableCell>
-                <TableCell component="th" scope="row">
-                  {new Date(entry.createdAt).toLocaleDateString()}{' '}
-                  {new Date(entry.createdAt).toLocaleTimeString()}
-                </TableCell>
-                <TableCell align="center" colSpan={1}>
-                  {entry.User.firstName} {entry.User.lastName}
-                </TableCell>
-                <TableCell
-                  align="center"
-                  sx={{
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                  }}
-                >
-                  <Box
-                    sx={[
-                      {
-                        backgroundColor:
-                          riskColors[
-                            entry.data.risks.find((r) => r.id === 'total')
-                              ?.level > 3
-                              ? 3
-                              : entry.data.risks.find((r) => r.id === 'total')
-                                  ?.level
-                          ],
-                      },
-                      resultStyles.tableCell,
-                    ]}
-                  >
-                    {entry.data.risks.find((r) => r.id === 'total')?.level}
-                  </Box>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Box>
-  )
+  const tableValues: TableValues[] = entriesWithData.map((entry) => ({
+    projectName: entry.data.answers[3],
+    date: `${new Date(entry.createdAt).toLocaleDateString()} ${new Date(
+      entry.createdAt
+    ).toLocaleTimeString()}`,
+    user: `${entry.User.firstName} ${entry.User.lastName}`,
+    total: entry.data.risks.find((r) => r.id === 'total')?.level,
+  }))
+
+  return <Table tableValues={tableValues} />
 }
 
 export default Summary
