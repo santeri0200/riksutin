755e4e6c84e85f6b0366073dbab2e2fdc5fbb4aa
erikao1998 <ersku1998@gmail.com>
2024-06-10 11:46:02 +0300

    Wrap sanctions fetching in try-catch block

--- src/server/data/sanctions/sanctionsMap.ts
+++ src/server/data/sanctions/sanctionsMap.ts
@@ -1,17 +1,21 @@
 const fetchSanctionsData = async (code: string | undefined) => {
   if (!code) return null
 
-  const url = 'https://sanctionsmap.eu/api/v1/regime'
-  const res = await fetch(url)
-  const data = await res.json()
+  try {
+    const url = 'https://sanctionsmap.eu/api/v1/regime'
+    const res = await fetch(url)
+    const data = await res.json()
 
-  const countrySanctions = data.data.find(
-    (c: any) => c.country.data.code === code
-  )?.has_lists
+    const countrySanctions = data.data.find(
+      (c: any) => c.country.data.code === code
+    )?.has_lists
 
-  if (!countrySanctions) return null
+    if (!countrySanctions) return null
 
-  return countrySanctions
+    return countrySanctions
+  } catch (error) {
+    return null
+  }
 }
 
 export default fetchSanctionsData
