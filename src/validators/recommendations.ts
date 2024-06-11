092e3badeb648ee0b937a946cd1b3010019c4073
erikao1998 <ersku1998@gmail.com>
2024-06-11 12:57:45 +0300

    Restore validators because tests fail and they might acually still be needed

---
+++ src/validators/recommendations.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod'
+
+export const NewRecommendationZod = z.object({
+  label: z.string().nonempty(),
+  type: z.string().nonempty(),
+  title: z.object({
+    fi: z.string().nonempty(),
+    sv: z.string().nonempty(),
+    en: z.string().nonempty(),
+  }),
+  text: z.object({
+    fi: z.string().nonempty(),
+    sv: z.string().nonempty(),
+    en: z.string().nonempty(),
+  }),
+})
+
+export type NewRecommendation = z.infer<typeof NewRecommendationZod>
+
+export const UpdatedRecommendationZod = z.object({
+  title: z.object({
+    fi: z.string().nonempty(),
+    sv: z.string().nonempty(),
+    en: z.string().nonempty(),
+  }),
+  text: z.object({
+    fi: z.string().nonempty(),
+    sv: z.string().nonempty(),
+    en: z.string().nonempty(),
+  }),
+})
+
+export type UpdatedRecommendation = z.infer<typeof UpdatedRecommendationZod>
