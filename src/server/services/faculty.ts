b92ae26edada322dabb40f772943fe5d06348de1
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2024-12-02 00:28:38 +0200

    QoL: Avoid making Jami calls in development

--- src/server/services/faculty.ts
+++ src/server/services/faculty.ts
@@ -1,11 +1,11 @@
 import type { Faculty } from '@types'
-import { inE2EMode } from '../util/config'
+import { inDevelopment, inE2EMode } from '../util/config'
 import mockFaculty from '../mocs/faculty'
 
 import { getOrganisationData, getUserOrganisations } from '../util/jami'
 
 export const getFaculties = async (): Promise<Faculty[]> => {
-  if (inE2EMode) return mockFaculty
+  if (inDevelopment || inE2EMode) return mockFaculty
 
   const organisationData = (await getOrganisationData()) || []
 
@@ -18,7 +18,7 @@ export const getUserFaculties = async (
   userId: string,
   iamGroups: string[]
 ): Promise<Faculty[]> => {
-  if (inE2EMode) return mockFaculty
+  if (inDevelopment || inE2EMode) return mockFaculty
 
   if (!userId) return []
 
