b39a72903e05e351cb23d5fa13250a557d4ef394
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-02-03 18:03:48 +0200

    Added fallback to DATABASE_URL

--- src/server/util/config.ts
+++ src/server/util/config.ts
@@ -10,7 +10,8 @@ export const { API_TOKEN, OPENAI_API_KEY } = process.env
 
 export const SESSION_SECRET = process.env.SESSION_SECRET || ''
 
-export const DATABASE_URL = process.env.DATABASE_URL || ''
+export const DATABASE_URL =
+  process.env.DATABASE_URL ?? 'postgres://postgres:@localhost/riksutin'
 
 export const REDIS_HOST = process.env.REDIS_HOST || 'redis'
 
