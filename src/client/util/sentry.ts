76d804f5adcb755e2ce4454d65b1fbbf2691b09f
Santeri Rajamäki <santeri.rajamaki@helsinki.fi>
2025-02-08 20:31:16 +0200

    Added prefs.json and reassigned values

--- src/client/util/sentry.ts
+++ src/client/util/sentry.ts
@@ -1,13 +1,19 @@
 import * as Sentry from '@sentry/browser'
 import { Integrations } from '@sentry/tracing'
-import { inProduction, inStaging, inE2EMode, GIT_SHA } from '@config'
+import {
+  inProduction,
+  inStaging,
+  inE2EMode,
+  SENTRY_DNS,
+  SENTRY_GIT_SHA,
+} from '@config'
 
 const initializeSentry = () => {
   if (!inProduction || inStaging || inE2EMode) return
 
   Sentry.init({
-    dsn: 'https://144f4b59d562a36afcdca13de964eb9a@toska.cs.helsinki.fi/3',
-    release: GIT_SHA,
+    dsn: SENTRY_DNS,
+    release: SENTRY_GIT_SHA,
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: 1.0,
   })
